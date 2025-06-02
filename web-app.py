08ab745995edec8271e9bda3218042bd553fccf2
LeonardoFM73 <leonardo.fajar.mardika-2020@ftmm.unair.ac.id>
2025-06-02 10:48:51 +0700

    Mengganti model diabetes

--- web-app.py
+++ web-app.py
@@ -185,7 +185,7 @@ if submit:
             # Load 3 model ML
             model_stroke = joblib.load('export-model/stroke_rf_bayes_model_smote.pkl')
             model_jantung = joblib.load('export-model/lr_jantung_smoteenn.pkl')
-            model_diabetes = joblib.load('export-model/Deteksi_diabetes_NN2.pkl')
+            model_diabetes = joblib.load('export-model/Deteksi_diabetes_NN.pkl')
 
             # Prediksi
             prediksi_stroke = model_stroke.predict(df_stroke)
